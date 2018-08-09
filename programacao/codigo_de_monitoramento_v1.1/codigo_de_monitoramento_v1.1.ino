1bbc5e5538e89f9cf1ef58958c49b0884c6c1505
Jonas <jonashenrique989@gmail.com>
2018-08-09 20:27:41 -0300

    Up de 1.1 e imagem de consumo energia

--- programacao/codigo_de_monitoramento_v1.1/codigo_de_monitoramento_v1.1.ino
+++ programacao/codigo_de_monitoramento_v1.1/codigo_de_monitoramento_v1.1.ino
@@ -3,11 +3,11 @@
  * Instituto Federal de Educação, Ciência e Tecnologia Minas Gerais
  * IFMG - Campus Avançado Conselheiro Lafaiete 
  * 
- * Código para monitoramento de pluviógrafo Versão 1.1
+ * Código para monitoramento de pluviógrafo Versão 1.1.1
  * 
  * Autor.............: Jonas Henrique Nascimento
  * Data de início....: 30/06/2018
- * Data da ultima atualização: 02/08/2018
+ * Data da ultima atualização: 05/08/2018
  * Data de término...: 22/07/2018
  * 
  * O código consiste em um leitor de dois botões que recebem nível lógico zero ao serem pressionados pelo movimento da báscula.
@@ -29,7 +29,7 @@
  */
 
 
------------------------------------------------------------------
+//-----------------------------------------------------------------
 // §§§§ Hardware §§§§  //
 /* 
  * Consiste na comunicação SPI com o módulo cartão SD, na comuni-
@@ -44,7 +44,7 @@
 */
 
 
------------------------------------------------------------------
+//-----------------------------------------------------------------
 // §§§§ Declaração das bibliotecas §§§§  //
 #include <avr/sleep.h>
 #include <DS3231.h>
@@ -52,15 +52,17 @@
 #include <SD.h>
 
 
------------------------------------------------------------------
+//-----------------------------------------------------------------
 // §§§§ Definições de Hardware §§§§  //
 #define entrada1 7
 #define entrada2 6
 #define saida    5
+#define interrupcao 2
 #define chip_select 4
+#define shield_RTC 8 
+#define shield_SD 9
 
-
------------------------------------------------------------------
+//-----------------------------------------------------------------
 // §§§§ Declaração das variáveis globais §§§§  //
 bool estado1 = false;
 bool estado2 = false;
@@ -69,22 +71,34 @@ bool flag2   = false;
 
 unsigned int minuto_antigo = 0;
 unsigned int minuto_atual = 0;
+unsigned int contador_de_interrupcao = 0;
 unsigned long contador_de_pulsos = 0;
 
 
------------------------------------------------------------------
+//-----------------------------------------------------------------
 // §§§§ Declaração dos objetos §§§§  //
 DS3231  rtc(SDA, SCL);
 Time  t;
 File datalogger;
 
 
------------------------------------------------------------------
+//-----------------------------------------------------------------
+// §§§§ Declaração de funcoes adicionais §§§§  //
+void comecando_a_dormir();
+void acordando();
+
+
+//-----------------------------------------------------------------
 // §§§§ Void Setup §§§§  //
 void setup() {
+   delay(200);
    pinMode(entrada1, INPUT_PULLUP);
    pinMode(entrada2, INPUT_PULLUP);
+   pinMode(interrupcao, INPUT);
    pinMode(saida, OUTPUT);
+   pinMode(shield_RTC, OUTPUT);
+   pinMode(shield_SD, OUTPUT);
+   pinMode(3, OUTPUT);
    Serial.begin(115200);
    int i = 150;
    rtc.begin();
@@ -92,6 +106,7 @@ void setup() {
    
    if (!SD.begin(chip_select)) {
      Serial.println("Erro ao ler cartao de memoria");
+     digitalWrite(3, HIGH);
      return;
   }
    //rtc.setDOW(FRIDAY);          // Selecione o dia em ingles; Ex: (SUNDAY) - Domingo
@@ -110,94 +125,132 @@ void setup() {
    digitalWrite(saida, HIGH);
    delay(500);
    digitalWrite(saida, LOW);
-
+   attachInterrupt(0, acordando, RISING);
 }
 
 
------------------------------------------------------------------
+//-----------------------------------------------------------------
 // §§§§ Void Loop §§§§  //
 void loop() {
 
- t = rtc.getTime();
+   digitalWrite(shield_SD, HIGH);
+   digitalWrite(shield_RTC, HIGH);
+   digitalWrite(3, HIGH);
 
- minuto_atual = t.min;
+   t = rtc.getTime();
+   minuto_atual = t.min;
 
- if ( minuto_antigo != minuto_atual ){
-    minuto_antigo = minuto_atual;
-    Serial.print("Foi apertado o botao: ");
-    Serial.println(contador_de_pulsos);
-    datalogger = SD.open("Valores.svc", FILE_WRITE);
-    if ( datalogger ) {
-       Serial.println("Atualizando datalogger");
-//       datalogger.println("    Data,    |    Hora,    |   Contagem,   |   Temperatura,   |");
-       datalogger.print(rtc.getDateStr());
-       datalogger.print(",  |  ");
-       datalogger.print(rtc.getTimeStr());
-       datalogger.print(",  |      ");
-       if(contador_de_pulsos < 10) {datalogger.print("0");}
-       datalogger.print(contador_de_pulsos);
-       datalogger.print(",       |      ");
-       datalogger.print(rtc.getTemp());
-       datalogger.println(",      |");
-       datalogger.close();
-    } else {
-       Serial.println("Erro ao abrir datalogger");
-    }
-    contador_de_pulsos = 0;
-    Serial.println("Atualizado");
- }
+   if ( minuto_antigo != minuto_atual ){  
+      minuto_antigo = minuto_atual;
+      Serial.print("Foi apertado o botao: ");
+      Serial.println(contador_de_pulsos);
+    
+      if (contador_de_pulsos == 0) {
+         contador_de_interrupcao++;
+      } else {
+         contador_de_interrupcao = 0;
+      }
+    
+      datalogger = SD.open("Valores.svc", FILE_WRITE);
+      if ( datalogger ) {
+         Serial.println("Atualizando datalogger");
+//         datalogger.println("    Data,    |    Hora,    |   Contagem,   |   Temperatura,   |");
+         datalogger.print(rtc.getDateStr());
+         datalogger.print(",  |  ");
+         datalogger.print(rtc.getTimeStr());
+         datalogger.print(",  |      ");
+         if(contador_de_pulsos < 10) {datalogger.print("0");}
+         datalogger.print(contador_de_pulsos);
+         datalogger.print(",       |      ");
+         datalogger.print(rtc.getTemp());
+         datalogger.println(",      |");
+         datalogger.close();
+      } else {
+         Serial.println("Erro ao abrir datalogger");
+      }
+      contador_de_pulsos = 0;
+      Serial.println("Atualizado");
+   }
 
- estado1 = digitalRead(entrada1);
- estado2 = digitalRead(entrada2);
+   estado1 = digitalRead(entrada1);
+   estado2 = digitalRead(entrada2);
 
- if ( estado1 == LOW ) {
-    flag1 = true;
- }
+   if ( estado1 == LOW ) {
+      flag1 = true;
+   }
 
- if ( estado2 == LOW ) {
-    flag2 = true;
- }
+   if ( estado2 == LOW ) {
+      flag2 = true;
+   }
 
- if ( (estado1 == HIGH) && (flag1 == true) ){ // botão 1 trocou de estado -- botao apertado
-    digitalWrite(saida, LOW);
-    contador_de_pulsos ++;
-    digitalWrite(saida, HIGH);
-    delay(20);
-    digitalWrite(saida, LOW);
-    delay(20);
-    flag1 = false;
- }
+   if ( (estado1 == HIGH) && (flag1 == true) ){ // botão 1 trocou de estado -- botao apertado
+      digitalWrite(saida, LOW);
+      contador_de_pulsos ++;
+      digitalWrite(saida, HIGH);
+      delay(20);
+      digitalWrite(saida, LOW);
+      delay(20);
+      flag1 = false;
+   }
 
- if ( (estado2 == HIGH) && (flag2 == true) ){ // botão 2 trocou de estado -- botao apertado
-    digitalWrite(saida, HIGH);
-    contador_de_pulsos ++;
-    digitalWrite(saida, HIGH);
-    delay(20);
-    digitalWrite(saida, LOW);
-    delay(20);
-    flag2 = false;
- }
+   if ( (estado2 == HIGH) && (flag2 == true) ){ // botão 2 trocou de estado -- botao apertado
+      digitalWrite(saida, HIGH);
+      contador_de_pulsos ++;
+      digitalWrite(saida, HIGH);
+      delay(20);
+      digitalWrite(saida, LOW);
+      delay(20);
+      flag2 = false;
+   }
 
- if ((estado1 == HIGH) && (estado2 == HIGH) || (flag1 == true) && (flag2 == true)) { // Erro, pois não se pode ter os dois botões ou as duas flags setadas ao mesmo tempo.
-    Serial.println("Erro lógico, Duas entradas em HIGH");
-    datalogger = SD.open ("Valores.svc, FILE_WRITE");
-    if (datalogger) {
-       datalogger.print("______________________________________________________________|");
-       datalogger.print(rtc.getDateStr());
-       datalogger.print(",  |  ");
-       datalogger.print(rtc.getTimeStr());
-       datalogger.print(",  |      ");
-       if(contador_de_pulsos < 10) {datalogger.print("0");}
-       datalogger.print(contador_de_pulsos);
-       datalogger.print(",       |      ");
-       datalogger.print(rtc.getTemp());
-       datalogger.println(",      |");
-       datalogger.print("Erro lógico, Duas entradas em HIGH");
-       datalogger.print("______________________________________________________________|");
-       datalogger.close();
-    }
- }
+   if ((estado1 == HIGH) && (estado2 == HIGH) || (flag1 == true) && (flag2 == true)) { // Erro, pois não se pode ter os dois botões ou as duas flags setadas ao mesmo tempo.
+      Serial.println("Erro lógico, Duas entradas em HIGH");
+      datalogger = SD.open ("Valores.svc, FILE_WRITE");
+      if (datalogger) {
+         datalogger.print("______________________________________________________________|");
+         datalogger.print(rtc.getDateStr());
+         datalogger.print(",  |  ");
+         datalogger.print(rtc.getTimeStr());
+         datalogger.print(",  |      ");
+         if(contador_de_pulsos < 10) {datalogger.print("0");}
+         datalogger.print(contador_de_pulsos);
+         datalogger.print(",       |      ");
+         datalogger.print(rtc.getTemp());
+         datalogger.println(",      |");
+         datalogger.print("Erro lógico, Duas entradas em HIGH");
+         datalogger.print("______________________________________________________________|");
+         datalogger.close();
+      }
+   }
 
+   if (contador_de_interrupcao >= 3) {
+      contador_de_interrupcao = 0;
+      Serial.println("Comecando a dormir");
+      comecando_a_dormir();
+   }
+
+} // fim loop
+
+
+void acordando (){
+   delayMicroseconds(200000);
+ //  digitalWrite(shield_SD, HIGH);
+ //  digitalWrite(shield_RTC, HIGH);
+ //  digitalWrite(3, HIGH);
+   delayMicroseconds(200000);
+}
+
+
+void comecando_a_dormir(){
+   digitalWrite(3, LOW);
+   digitalWrite(shield_SD, LOW);
+   digitalWrite(shield_RTC, LOW);
+   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+   sleep_enable();
+   attachInterrupt(0, acordando, RISING);
+   sleep_mode();
+   sleep_disable();
+   detachInterrupt(0);
 }
 
 
